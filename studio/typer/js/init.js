8c9628a2477c22106806cd65ee9d70b8f9ea5ebf
YIqi17 <42971110+YIqi17@users.noreply.github.com>
2019-03-08 10:18:40 -0800

    adding class name

--- studio/typer/js/init.js
+++ studio/typer/js/init.js
@@ -98,33 +98,33 @@ function createElement(k) {
     }
     var elem = $('#cursor');
 
-    if (k == "a" || k == "A") { elem.before('<span><img tag="a" src="assets/a.svg" class="svg"></span>'); }
-    if (k == "b" || k == "B") { elem.before('<span><img tag="b" src="assets/b.svg" class="svg"></span>'); }
-    if (k == "c" || k == "C") { elem.before('<span><img tag="c" src="assets/c.svg" class="svg"></span>'); }
-    if (k == "d" || k == "D") { elem.before('<span><img tag="d" src="assets/d.svg" class="svg"></span>'); }
-    if (k == "e" || k == "E") { elem.before('<span><img tag="e" src="assets/e.svg" class="svg"></span>'); }
-    if (k == "f" || k == "F") { elem.before('<span><img tag="f"src="assets/f.svg"  class="svg"></span>'); }
-    if (k == "g" || k == "G") { elem.before('<span><img tag="g" src="assets/g.svg" class="svg"></span>'); }
-    if (k == "h" || k == "H") { elem.before('<span><img tag="h" src="assets/h.svg" class="svg"></span>'); }
-    if (k == "i" || k == "I") { elem.before('<span><img tag="i" src="assets/i.svg" class="svg"></span>'); }
-    if (k == "j" || k == "J") { elem.before('<span><img tag="j" src="assets/j.svg" class="svg"></span>'); }
-    if (k == "k" || k == "K") { elem.before('<span><img tag="k" src="assets/k.svg" class="svg"></span>'); }
-    if (k == "l" || k == "L") { elem.before('<span><img tag="l" src="assets/l.svg" class="svg"></span>'); }
-    if (k == "m" || k == "M") { elem.before('<span><img tag="m" src="assets/m.svg" class="svg"></span>'); }
-    if (k == "n" || k == "N") { elem.before('<span><img tag="n" src="assets/n.svg" class="svg"></span>'); }
-    if (k == "o" || k == "O") { elem.before('<span><img tag="o" src="assets/o.svg" class="svg"></span>'); }
-    if (k == "p" || k == "P") { elem.before('<span><img tag="p" src="assets/p.svg" class="svg"></span>'); }
-    if (k == "q" || k == "Q") { elem.before('<span><img tag="q" src="assets/q.svg" class="svg"></span>'); }
-    if (k == "r" || k == "R") { elem.before('<span><img tag="r" src="assets/r.svg" class="svg"></span>'); }
-    if (k == "s" || k == "S") { elem.before('<span><img tag="s" src="assets/s.svg" class="svg"></span>'); }
-    if (k == "t" || k == "T") { elem.before('<span><img tag="t" src="assets/t.svg" class="svg"></span>'); }
-    if (k == "u" || k == "U") { elem.before('<span><img tag="u" src="assets/u.svg" class="svg"></span>'); }
-    if (k == "v" || k == "V") { elem.before('<span><img tag="v" src="assets/v.svg" class="svg"></span>'); }
-    if (k == "w" || k == "W") { elem.before('<span><img tag="w" src="assets/w.svg" class="svg"></span>'); }
-    if (k == "x" || k == "X") { elem.before('<span><img tag="x" src="assets/x.svg" class="svg"></span>'); }
-    if (k == "y" || k == "Y") { elem.before('<span><img tag="y" src="assets/y.svg" class="svg"></span>'); }
-    if (k == "z" || k == "Z") { elem.before('<span><img tag="z" src="assets/z.svg" class="svg"></span>'); }
-    if (k == " ") { elem.before('<span>&nbsp;</span>'); }
+    if (k == "a" || k == "A") { elem.before('<span class="inner"><img tag="a" src="assets/a.svg" class="svg"></span>'); }
+    if (k == "b" || k == "B") { elem.before('<span class="inner"><img tag="b" src="assets/b.svg" class="svg"></span>'); }
+    if (k == "c" || k == "C") { elem.before('<span class="inner"><img tag="c" src="assets/c.svg" class="svg"></span>'); }
+    if (k == "d" || k == "D") { elem.before('<span class="inner"><img tag="d" src="assets/d.svg" class="svg"></span>'); }
+    if (k == "e" || k == "E") { elem.before('<span class="inner"><img tag="e" src="assets/e.svg" class="svg"></span>'); }
+    if (k == "f" || k == "F") { elem.before('<span class="inner"><img tag="f"src="assets/f.svg"  class="svg"></span>'); }
+    if (k == "g" || k == "G") { elem.before('<span class="inner"><img tag="g" src="assets/g.svg" class="svg"></span>'); }
+    if (k == "h" || k == "H") { elem.before('<span class="inner"><img tag="h" src="assets/h.svg" class="svg"></span>'); }
+    if (k == "i" || k == "I") { elem.before('<span class="inner"><img tag="i" src="assets/i.svg" class="svg"></span>'); }
+    if (k == "j" || k == "J") { elem.before('<span class="inner"><img tag="j" src="assets/j.svg" class="svg"></span>'); }
+    if (k == "k" || k == "K") { elem.before('<span class="inner"><img tag="k" src="assets/k.svg" class="svg"></span>'); }
+    if (k == "l" || k == "L") { elem.before('<span class="inner"><img tag="l" src="assets/l.svg" class="svg"></span>'); }
+    if (k == "m" || k == "M") { elem.before('<span class="inner"><img tag="m" src="assets/m.svg" class="svg"></span>'); }
+    if (k == "n" || k == "N") { elem.before('<span class="inner"><img tag="n" src="assets/n.svg" class="svg"></span>'); }
+    if (k == "o" || k == "O") { elem.before('<span class="inner"><img tag="o" src="assets/o.svg" class="svg"></span>'); }
+    if (k == "p" || k == "P") { elem.before('<span class="inner"><img tag="p" src="assets/p.svg" class="svg"></span>'); }
+    if (k == "q" || k == "Q") { elem.before('<span class="inner"><img tag="q" src="assets/q.svg" class="svg"></span>'); }
+    if (k == "r" || k == "R") { elem.before('<span class="inner"><img tag="r" src="assets/r.svg" class="svg"></span>'); }
+    if (k == "s" || k == "S") { elem.before('<span class="inner"><img tag="s" src="assets/s.svg" class="svg"></span>'); }
+    if (k == "t" || k == "T") { elem.before('<span class="inner"><img tag="t" src="assets/t.svg" class="svg"></span>'); }
+    if (k == "u" || k == "U") { elem.before('<span class="inner"><img tag="u" src="assets/u.svg" class="svg"></span>'); }
+    if (k == "v" || k == "V") { elem.before('<span class="inner"><img tag="v" src="assets/v.svg" class="svg"></span>'); }
+    if (k == "w" || k == "W") { elem.before('<span class="inner"><img tag="w" src="assets/w.svg" class="svg"></span>'); }
+    if (k == "x" || k == "X") { elem.before('<span class="inner"><img tag="x" src="assets/x.svg" class="svg"></span>'); }
+    if (k == "y" || k == "Y") { elem.before('<span class="inner"><img tag="y" src="assets/y.svg" class="svg"></span>'); }
+    if (k == "z" || k == "Z") { elem.before('<span class="inner"><img tag="z" src="assets/z.svg" class="svg"></span>'); }
+    if (k == " ") { elem.before('<span class="inner">&nbsp;</span>'); }
     if (k == "1") {
         helpers.removeClass('animated');
         $(".svg").css("filter", "hue-rotate(180deg) saturate(4) brightness(0.9)");
