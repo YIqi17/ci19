24f21147663eb492b9a3ee2d0b13ad3ee4e2b22a
YIqi17 <42971110+YIqi17@users.noreply.github.com>
2019-03-08 08:18:07 -0800

    added one more braille function

--- studio/typer/js/init.js
+++ studio/typer/js/init.js
@@ -1,3 +1,29 @@
+function changeAll(){
+    var imgs = document.querySelectorAll("img");
+    for(var i = 0; i < imgs.length; i++){
+        console.log(imgs[i].getAttribute("tag"));
+        var tagname = imgs[i].getAttribute("tag");
+        imgs[i].src = "braille_" + tagname + ".svg"
+        console.log(imgs[i]);
+        console.log(imgs[i].src);
+//        var node = imgs[i];
+//        while(node.firstChild){
+//            node.removeChild(node.firstChild);
+//        }
+    }
+}
+function replaceCurrent(){
+    var imgs = document.querySelectorAll("img");
+    for(var i = 0; i < imgs.length; i++){
+        console.log(imgs[i].getAttribute("tag"));
+        var tagname = imgs[i].getAttribute("tag");
+        imgs[i].src = "assets/" + tagname + ".svg"
+        console.log(imgs[i]);
+        console.log(imgs[i].src);
+    }
+}
+
+var keycount = 0;
 
 $(document).ready(function () {
     var entryCount = 0;
@@ -32,34 +58,73 @@ $(document).ready(function () {
 });
 
 function createElement(k) {
+
+    keycount += 1;
+    if(keycount === 10){
+        changeAll();
+    }
+    if(keycount === 11){
+        replaceCurrent();
+    }
+    if(keycount === 30){
+        changeAll();
+    }
+    if(keycount === 33){
+        replaceCurrent();
+    }
+    if(keycount === 50){
+        changeAll();
+    }
+    if(keycount === 55){
+        replaceCurrent();
+    }
+    if(keycount ===100){
+        changeAll();
+    }
+    if(keycount ===110){
+        replaceCurrent();
+    }
+    if(keycount === 200){
+        changeAll();
+    }
+    if(keycount === 215){
+        replaceCurrent();
+    }
+    if(keycount === 400){
+        changeAll();
+    }
+    if(keycount === 420){
+        replaceCurrent();
+    }
     var elem = $('#cursor');
-    if (k == "a" || k == "A") { elem.before('<span class="inner"><embed src="assets/a.svg" class="svg"></span>'); }
-    if (k == "b" || k == "B") { elem.before('<span class="inner"><embed src="assets/b.svg" class="svg"></span>'); }
-    if (k == "c" || k == "C") { elem.before('<span class="inner"><embed src="assets/c.svg" class="svg"></span>'); }
-    if (k == "d" || k == "D") { elem.before('<span class="inner"><embed src="assets/d.svg" class="svg"></span>'); }
-    if (k == "e" || k == "E") { elem.before('<span class="inner"><embed src="assets/e.svg" class="svg"></span>'); }
-    if (k == "f" || k == "F") { elem.before('<span class="inner"><embed src="assets/f.svg" class="svg"></span>'); }
-    if (k == "g" || k == "G") { elem.before('<span class="inner"><embed src="assets/g.svg" class="svg"></span>'); }
-    if (k == "h" || k == "H") { elem.before('<span class="inner"><embed src="assets/h.svg" class="svg"></span>'); }
-    if (k == "i" || k == "I") { elem.before('<span class="inner"><embed src="assets/i.svg" class="svg"></span>'); }
-    if (k == "j" || k == "J") { elem.before('<span class="inner"><embed src="assets/j.svg" class="svg"></span>'); }
-    if (k == "k" || k == "K") { elem.before('<span class="inner"><embed src="assets/k.svg" class="svg"></span>'); }
-    if (k == "l" || k == "L") { elem.before('<span class="inner"><embed src="assets/l.svg" class="svg"></span>'); }
-    if (k == "m" || k == "M") { elem.before('<span class="inner"><embed src="assets/m.svg" class="svg"></span>'); }
-    if (k == "n" || k == "N") { elem.before('<span class="inner"><embed src="assets/n.svg" class="svg"></span>'); }
-    if (k == "o" || k == "O") { elem.before('<span class="inner"><embed src="assets/o.svg" class="svg"></span>'); }
-    if (k == "p" || k == "P") { elem.before('<span class="inner"><embed src="assets/p.svg" class="svg"></span>'); }
-    if (k == "q" || k == "Q") { elem.before('<span class="inner"><embed src="assets/q.svg" class="svg"></span>'); }
-    if (k == "r" || k == "R") { elem.before('<span class="inner"><embed src="assets/r.svg" class="svg"></span>'); }
-    if (k == "s" || k == "S") { elem.before('<span class="inner"><embed src="assets/s.svg" class="svg"></span>'); }
-    if (k == "t" || k == "T") { elem.before('<span class="inner"><embed src="assets/t.svg" class="svg"></span>'); }
-    if (k == "u" || k == "U") { elem.before('<span class="inner"><embed src="assets/u.svg" class="svg"></span>'); }
-    if (k == "v" || k == "V") { elem.before('<span class="inner"><embed src="assets/v.svg" class="svg"></span>'); }
-    if (k == "w" || k == "W") { elem.before('<span class="inner"><embed src="assets/w.svg" class="svg"></span>'); }
-    if (k == "x" || k == "X") { elem.before('<span class="inner"><embed src="assets/x.svg" class="svg"></span>'); }
-    if (k == "y" || k == "Y") { elem.before('<span class="inner"><embed src="assets/y.svg" class="svg"></span>'); }
-    if (k == "z" || k == "Z") { elem.before('<span class="inner"><embed src="assets/z.svg" class="svg"></span>'); }
-    if (k == " ") { elem.before('<span class="inner">&nbsp;</span>'); }
+
+    if (k == "a" || k == "A") { elem.before('<span><img tag="a" src="assets/a.svg" class="svg"></span>'); }
+    if (k == "b" || k == "B") { elem.before('<span><img tag="b" src="assets/b.svg" class="svg"></span>'); }
+    if (k == "c" || k == "C") { elem.before('<span><img tag="c" src="assets/c.svg" class="svg"></span>'); }
+    if (k == "d" || k == "D") { elem.before('<span><img tag="d" src="assets/d.svg" class="svg"></span>'); }
+    if (k == "e" || k == "E") { elem.before('<span><img tag="e" src="assets/e.svg" class="svg"></span>'); }
+    if (k == "f" || k == "F") { elem.before('<span><img tag="f"src="assets/f.svg"  class="svg"></span>'); }
+    if (k == "g" || k == "G") { elem.before('<span><img tag="g" src="assets/g.svg" class="svg"></span>'); }
+    if (k == "h" || k == "H") { elem.before('<span><img tag="h" src="assets/h.svg" class="svg"></span>'); }
+    if (k == "i" || k == "I") { elem.before('<span><img tag="i" src="assets/i.svg" class="svg"></span>'); }
+    if (k == "j" || k == "J") { elem.before('<span><img tag="j" src="assets/j.svg" class="svg"></span>'); }
+    if (k == "k" || k == "K") { elem.before('<span><img tag="k" src="assets/k.svg" class="svg"></span>'); }
+    if (k == "l" || k == "L") { elem.before('<span><img tag="l" src="assets/l.svg" class="svg"></span>'); }
+    if (k == "m" || k == "M") { elem.before('<span><img tag="m" src="assets/m.svg" class="svg"></span>'); }
+    if (k == "n" || k == "N") { elem.before('<span><img tag="n" src="assets/n.svg" class="svg"></span>'); }
+    if (k == "o" || k == "O") { elem.before('<span><img tag="o" src="assets/o.svg" class="svg"></span>'); }
+    if (k == "p" || k == "P") { elem.before('<span><img tag="p" src="assets/p.svg" class="svg"></span>'); }
+    if (k == "q" || k == "Q") { elem.before('<span><img tag="q" src="assets/q.svg" class="svg"></span>'); }
+    if (k == "r" || k == "R") { elem.before('<span><img tag="r" src="assets/r.svg" class="svg"></span>'); }
+    if (k == "s" || k == "S") { elem.before('<span><img tag="s" src="assets/s.svg" class="svg"></span>'); }
+    if (k == "t" || k == "T") { elem.before('<span><img tag="t" src="assets/t.svg" class="svg"></span>'); }
+    if (k == "u" || k == "U") { elem.before('<span><img tag="u" src="assets/u.svg" class="svg"></span>'); }
+    if (k == "v" || k == "V") { elem.before('<span><img tag="v" src="assets/v.svg" class="svg"></span>'); }
+    if (k == "w" || k == "W") { elem.before('<span><img tag="w" src="assets/w.svg" class="svg"></span>'); }
+    if (k == "x" || k == "X") { elem.before('<span><img tag="x" src="assets/x.svg" class="svg"></span>'); }
+    if (k == "y" || k == "Y") { elem.before('<span><img tag="y" src="assets/y.svg" class="svg"></span>'); }
+    if (k == "z" || k == "Z") { elem.before('<span><img tag="z" src="assets/z.svg" class="svg"></span>'); }
+    if (k == " ") { elem.before('<span>&nbsp;</span>'); }
     if (k == "1") {
         helpers.removeClass('animated');
         $(".svg").css("filter", "hue-rotate(180deg) saturate(4) brightness(0.9)");
@@ -114,7 +179,7 @@ function createElement(k) {
     }
     if (k == "0") {
         helpers.removeClass('animated');
-        document.getElementsByClassName('.svg').classList.add('changefilter');
+        document.getElementsByTagName("span").setAttribute("id", "changefilter");
     }
 }
 function deleteElement() {
@@ -152,8 +217,16 @@ const helpers = {
 
         }
     }
-}
+// }
+// function generateRandomImage() {
+//     var randomimages = ["assets/a.svg", "assets/b.svg", "assets/c.svg"];
+//     console.log(choices);
+//     var randd = Math.floor(Math.random() * 3);
+//     console.log(randomNumber);
+//     document.querySelector("#image").createElement = randomimages[randd];
+// }
+
 // function generateFilter(){
 //     var h = rand(1, 360);
 //     return 'hue-rotate(' + h + 'deg)';
-// }
+}
